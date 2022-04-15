5ee8791c01c60f8bd9758dda8792d7c7544d35a1
ellias <ellias.chabloz@gmail.com>
2022-04-15 10:57:55 +0200

    corrections

--- Serious Game on Ethics 2022/scenes/MainMenu.gd
+++ Serious Game on Ethics 2022/scenes/MainMenu.gd
@@ -43,6 +43,6 @@ func _on_CreditsButton_pressed():
 
 func _on_SecondChapter_pressed():
 	Dialogic.reset_saves()
-	Dialogic.set_current_timeline('II_0_0_start_second_chapter')
+	Dialogic.set_current_timeline('II_2_1_labo')
 	load_main_game()
 	
